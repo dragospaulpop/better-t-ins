ac5fa6a9cb0d31902f7d2369d3f4f7313996c531
DRagos-Paul Pop <froind@gmail.com>
2025-12-10 19:20:22 +0200

    prepared for first deployment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3001,
-    allowedHosts: ["app.tud-box.test", "localhost"],
+    allowedHosts: ["app.tud-box.test", "localhost", "tud-box.verdedata.ro"],
     // https: {
     //   key: "./.certs/app.tud-box.test+6-key.pem",
     //   cert: "./.certs/app.tud-box.test+6.pem",
